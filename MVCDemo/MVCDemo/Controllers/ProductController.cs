e7166020b9c98baf8d496b54edc0d5cf574c939c
Yuri Ognyanov <manushevit@gmail.com>
2024-01-20 22:07:12 +0200

    Added new Controller and View Product

---
+++ MVCDemo/MVCDemo/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDemo.Models.Product;
+
+namespace MVCDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IEnumerable<ProductViewModel> products = new List<ProductViewModel>() 
+        { 
+           new ProductViewModel()
+           { Id = 1, 
+               Name = "Cheese", 
+               Price = 7.00M 
+           },
+           new ProductViewModel()
+           { Id = 2,
+               Name = "Ham",
+               Price = 5.50M
+           },
+           new ProductViewModel()
+           { Id = 3,
+               Name = "Bread",
+               Price = 1.50M
+           }
+        };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult All()
+        {
+            return View(products);
+        }
+    }
+}
